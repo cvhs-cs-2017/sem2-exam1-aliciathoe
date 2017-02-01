452defe7d6a413695d2439d2ad4f7f9866f5e90d
aliciathoe <aliciathoe1124@Gmail.com>
2017-02-01 11:28:44 -0800

    Update Turtle.py

--- Turtle.py
+++ Turtle.py
@@ -1,6 +1,30 @@
 """Create a Turtle Program that will draw a 3-dimensional cube"""
 
 
+import turtle
+sven = turtle.Turtle()
+
+for i in range(4):
+    sven.forward(45)
+    sven.right(90)
+sven.left(45)
+sven.forward(45)
+sven.right(45)
+for i in range(4):
+    sven.forward(45)
+    sven.right(90)
+sven.forward(45)
+sven.left(45)
+sven.backward(45)
+sven.right(135)
+for i in range(3):
+    sven.forward(45)
+    sven.left(135)
+sven.right(90)
+sven.forward(45)
+
+
+input()
 
 
 
