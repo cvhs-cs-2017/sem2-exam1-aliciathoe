10dab0324b1b40c61ef713777126579f55add69e
aliciathoe <aliciathoe1124@Gmail.com>
2017-02-01 11:26:29 -0800

    Update Encrypt.py

--- Encrypt.py
+++ Encrypt.py
@@ -4,7 +4,27 @@
 
 Print the save the result as the variable = NoVowels
 """"
+def RemoveVowels(x):
+    string = ""
+    x = x.lower()
+    for ch in x:
+        if ch is 'a':
+            newstring = newstring + ''
+        elif ch is 'e':
+            newstring = newstring + ''
+        elif ch is 'i':
+            newstring = newstring + ''
+        elif ch is 'o':
+            newstring = newstring + ''
+        elif ch is 'u':
+            newstring = newstring + ''
+        else:
+            newstring = newstring + ch
+    return newstring
 
+NoVowels = RemoveVowels("Computer Science Makes the World go round but it doesn't make the world round itself!")
+
+print(NoVowels)
 
 
 
